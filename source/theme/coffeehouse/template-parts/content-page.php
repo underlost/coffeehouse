1ac935fdecdab4f3afa39a643d8d2d44511978fe
Tyler Rilling <underlost@gmail.com>
2015-06-27 14:15:10 -0700

    updates

--- source/theme/coffeehouse/template-parts/content-page.php
+++ source/theme/coffeehouse/template-parts/content-page.php
@@ -7,23 +7,33 @@
 
 ?>
 
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+<div class="article-wrap">
+
+	<?php if ( has_post_thumbnail() ) { ?>
+	<div class="photo layout-single-column">
+	<?php $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), '_bellevue-large');
+	echo '<a href="' . $large_image_url[0] . '" title="' . the_title_attribute('echo=0') . '" >'; ?>
+	<figure><?php the_post_thumbnail('large');?></figure>
+	<?php echo '</a>'; ?> </div>
+	<?php } ?>
+
+	<header class="entry-header layout-single-column">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
+	<div class="entry-content layout-single-column">
 		<?php the_content(); ?>
 		<?php
 			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'coffeehouse' ),
+				'before' => '<div class="page-links">' . __( 'Pages:', 'seattle' ),
 				'after'  => '</div>',
 			) );
 		?>
 	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php edit_post_link( esc_html__( 'Edit', 'coffeehouse' ), '<span class="edit-link">', '</span>' ); ?>
+	<footer class="entry-footer layout-single-column">
+		<?php edit_post_link( __( 'Edit', 'seattle' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-footer -->
+</div>
 </article><!-- #post-## -->
-
--- source/theme/coffeehouse/template-parts/content.php
+++ source/theme/coffeehouse/template-parts/content.php
@@ -7,35 +7,16 @@
 
 ?>
 
+<?php if ( has_post_thumbnail() ) : ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>
+<?php $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'seattle-large');
+echo ' style="background-image: url(' . $large_image_url[0] . ')"'; ?>>
+<?php else : ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+<?php endif; ?>
+	<div class="article-wrap">
 
-		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php coffeehouse_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+	<?php $format = get_post_format(); get_template_part( 'format', $format ); ?>
 
-	<div class="entry-content">
-		<?php
-			/* translators: %s: Name of current post */
-			the_content( sprintf(
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'coffeehouse' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-		?>
-
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'coffeehouse' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php coffeehouse_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+	</div>
 </article><!-- #post-## -->
